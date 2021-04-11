9355aed47d7c999e9ef2e61538cab609872e15b7
jonatsts <jonatassantos96@hotmail.com>
2021-04-11 13:42:41 -0300

    Adicionada animação do logo 'react native'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -7,76 +7,57 @@ import {
   Text,
   useColorScheme,
   View,
-  Image
+  Image,
+  Easing,
+  Animated,
 } from 'react-native';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 import profile from './assets/profile.png';
+import logo from './assets/react-logo.png';
+import styles from './styles'
 
 const App = () => {
-  return (
-    <>
-      <SafeAreaView style={style.page}>
-        <View>
-          <Image source={profile} style={style.image} />
+  const [spinAnim, setSpinAnim] = useState(new Animated.Value(0));
+  const spin = spinAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
 
-          <Text style={style.name}>Jônatas Santos</Text>
-          <Text style={style.work}>React Native Developer</Text>
-        </View>
-        <View style={style.icons}>
-          <FontAwesomeIcon style={[style.icon, style.icon_github]} name="github" size={27} />
-          <FontAwesomeIcon style={style.icon} name="chrome" size={23} />
-          <FontAwesomeIcon style={style.icon} name="linkedin-square" size={23} />
-        </View>
-      </SafeAreaView>
-    </>
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(spinAnim, {
+        toValue: 1,
+        duration: 5000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+    ).start();
+  });
+
+  return (
+    <SafeAreaView style={styles.page}>
+      <View>
+        <Image source={profile} style={styles.image} />
+
+        <Text style={styles.name}>Jônatas Santos</Text>
+        <Text style={styles.work}>React Native Developer</Text>
+      </View>
+
+      <View style={styles.icons}>
+        <FontAwesomeIcon style={[styles.icon, styles.icon_github]} name="github" size={27} />
+        <FontAwesomeIcon style={styles.icon} name="chrome" size={23} />
+        <FontAwesomeIcon style={styles.icon} name="linkedin-square" size={23} />
+      </View>
+
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Animated.Image
+          style={{ height: 220, width: 220, transform: [{ rotate: spin }] }}
+          source={logo}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
-
-const style = StyleSheet.create({
-  page: {
-    backgroundColor: '#22272E',
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  },
-
-  image: {
-    marginTop: 120,
-    width: 190,
-    height: 190,
-    borderRadius: 125,
-    marginBottom: 12,
-  },
-
-  icon: {
-    color: '#FFF',
-    textAlign: 'center',
-    marginHorizontal: 12,
-    marginTop: 10,
-  },
-
-  icon_github: {
-    marginTop: 8,
-  },
-
-  icons: {
-    flexDirection: 'row',
-  },
-
-  name: {
-    color: '#FFF',
-    fontSize: 26,
-    marginTop: 11,
-    textAlign: 'center',
-  },
-
-  work: {
-    color: '#FFF',
-    marginBottom: 11,
-    textAlign: 'center',
-  }
-
-});
 
 export default App;
